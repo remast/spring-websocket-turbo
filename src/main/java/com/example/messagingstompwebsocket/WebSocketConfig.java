7294365b3bb0e7134f1fd8d894d1bec24e928234
Jan Stamer <jan.stamer@freenet.de>
2021-01-01 10:35:58 +0100

    Initial commit.

---
+++ src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new SocketHandler(), "/chat");
+	}
+
+}
---
+++ src/main/java/com/example/messagingstompwebsocket/WebsocketTurboApplication.java
@@ -0,0 +1,12 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTurboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketTurboApplication.class, args);
+	}
+}
